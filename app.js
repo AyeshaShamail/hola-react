7ed9043fee6088aed5736c6747e244706c153a22
AyeshaShamail <ayeshashamail1931@gmail.com>
2024-01-18 23:30:48 +0530

    Revising episode-1 inception

---
+++ app.js
@@ -0,0 +1,38 @@
+/*    <div id="parent">
+      <div id="child">
+        <h1>I am a heading made from React</h1>
+      </div>
+    </div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child" },
+    [
+      React.createElement("h1", {}, "This is a heading built from React"),
+      React.createElement(
+        "h1",
+        {},
+        "This is second heading which is sibling of 1st heading"
+      ),
+    ] //For siblings , keep it in array.
+  ),
+  React.createElement(
+    "div",
+    { id: "child" },
+    [
+      React.createElement("h1", {}, "This is a heading built from React"),
+      React.createElement(
+        "h1",
+        {},
+        "This is second heading which is sibling of 1st heading"
+      ),
+    ] //For siblings , keep it in array.
+  ),
+]);
+
+console.log("parent");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
