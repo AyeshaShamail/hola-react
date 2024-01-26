a4448691f0aa3188cd76d639cd2fd3c8519252df
AyeshaShamail <ayeshashamail1931@gmail.com>
2024-01-26 22:39:02 +0530

    Episode 5 revised yay!

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,20 +3,19 @@ import resList from "../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
-  //Super-powerful variable
+  //Super-powerful variable or state variable which are called hooks
   // setListOfRestaurants is used to update the list and is the second parameter of the array.
   const [ListOfRestaurants, setListOfRestaurants] = useState(resList);
 
   // Above is same as Array destructuring which is done below //
 
   // const arr = useState(resList);
-  // const [ListOfRestaurants, setListOfRestaurants] = arr; 
+  // const [ListOfRestaurants, setListOfRestaurants] = arr;
 
   // or same as //
-  
+
   // const ListOfRestaurants = arr[0];
   // const setListOfRestaurants = arr[1];
-   
 
   return (
     <div className="body">
